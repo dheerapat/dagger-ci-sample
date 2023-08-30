71e2fa08c62a1a2968b9940cbd910cdba0aa5945
Dheerapat Tookkane <dheerapat.t@kkumail.com>
2023-08-30 20:35:03 +0700

    dagger ci init

---
+++ ci/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	// use a node:16-slim container
+	// mount the source code directory on the host
+	// at /src in the container
+	source := client.Container().
+		From("node:lts-slim").
+		WithDirectory("/src", client.Host().Directory("."), dagger.ContainerWithDirectoryOpts{
+			Exclude: []string{"ci/"},
+		})
+
+	// set the working directory in the container
+	// install application dependencies
+	runner := source.WithWorkdir("/src")
+
+	// run application tests
+	out, err := runner.WithExec([]string{"npm", "test", "--", "--watchAll=false"}).
+		Stderr(ctx)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
+}
